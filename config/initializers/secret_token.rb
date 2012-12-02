877493df0636c0a2363b1c0de782de211ee9618a
Ubuntu 12.04 <neel@ubuntu.(none)>
2012-12-02 16:18:16 -0600

    initial foodapp commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodapp::Application.config.secret_token = 'd0b47dd33712b811c5610466c295e30377ad7dd2f3f6fdcfdad8bd0b88135452b91e06e2969ba91f42a380fcba3b6e98cc960543688628aaaa93e42ec2df3b09'

5822ede25b3b1065ca2cd00266a9d00692e9f07f
Ubuntu 12.04 <neel@ubuntu.(none)>
2012-12-11 21:23:28 -0600

    Added 3 person limit to User table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,18 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
 
-  	if @user.save
-      flash[:success] = "#{@user.name}, welcome to Neel's Food App!"
-  		redirect_to user_url(@user)
-  	else
-  		render 'new'
-  	end
-  		
+    if User.all.count >= 3
+      flash[:error] = "We have reached the maximum users allowed in beta mode. Please try
+                      back later"
+    else
+      if @user.save
+        flash[:success] = "#{@user.name}, welcome to Neel's Food App!"
+        redirect_to user_url(@user) 
+        return
+      end
+    end
+  
+  	render 'new'  		
   end
 
 end
